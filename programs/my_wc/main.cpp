f479810e376b64ba2da9c3acff51b42d92c3a5fa
bigjeff96 <joepep271996@gmail.com>
2023-02-12 03:54:32 +0100

    can sort of read stdin for wc

--- programs/my_wc/main.cpp
+++ programs/my_wc/main.cpp
@@ -11,40 +11,57 @@
 #define BOLD "\x1B[1m"
 #define RED_BACKGROUND "\x1B[41m"
 #define DEFAULT_BACKGROUND "\x1B[49m"
-typedef enum { COUNT_LINES, COUNT_WORDS, COUNT_BYTES, FLAGS_COUNT} FLAGS;
+typedef enum { COUNT_LINES, COUNT_WORDS, COUNT_BYTES, FLAGS_COUNT } FLAGS;
 
 gb_global b8 flags[BITNSLOTS(FLAGS_COUNT)] = {};
 
-int main(int argc, char** argv) {
-
-    // TODO: if no file is passed as arg, read stdin
+int main(int argc, char** argv)
+{
+    gbString buffer = {};
+    defer(gb_string_free(buffer));
     if (argc == 1) {
-        gb_printf_err("Usage: %s [file]\n", argv[0]);
-        gb_exit(1);
+        auto input_file = gb_file_get_standard(gbFileStandard_Input);
+        isize file_size = gb_file_size(input_file);
+        
+        if (!file_size) {
+            //FIXME: can't read piped data like 'head main.cpp | ./my_wc.exe', I get 0 file size
+            printf("ERROR: empty stdin\n");
+            gb_exit(1);
+        }
+        
+        buffer = gb_string_make_reserve(gb_heap_allocator(), file_size);
+        auto good = gb_file_read(input_file, buffer, gb_string_capacity(buffer));
+        GB_STRING_HEADER(buffer)->length = file_size;
+        if (!good) {
+            printf("ERROR:\n");
+            gb_exit(1);
+        }
+    } else {
+        auto file = gb_file_read_contents(gb_heap_allocator(), 1, argv[1]);
+        buffer = cast(char*) file.data;
+        GB_STRING_HEADER(buffer)->allocator = file.allocator;
+        GB_STRING_HEADER(buffer)->length = file.size;
+        GB_STRING_HEADER(buffer)->capacity = file.size;
     }
 
-    auto file = gb_file_read_contents(gb_heap_allocator(), 1, argv[1]);
-    defer(gb_file_free_contents(&file));
-
     isize total_lignes = 0;
-    isize total_bytes = file.size;
+    isize total_bytes = gb_string_length(buffer);
     isize total_words = 0;
     isize total_white_space = 0;
 
-    for (isize i = 0; i < file.size; i++) {
-        char* current_character = (char*)file.data + i;
-        
+    for (isize i = 0; i < gb_string_length(buffer); i++) {
+        char* current_character = buffer + i;
+
         if (*current_character == '\n')
             total_lignes++;
 
-        //TODO: make better method, since this ignores things like '(bob is dead)' and '}' is a word
+        // TODO: make better method, since this ignores things like '(bob is dead)' and '}' is a word
         if (gb_char_is_space(*current_character))
             total_white_space++;
     }
 
-    if (file.size > 0) {
+    if (gb_string_length(buffer) > 0)
         total_words = total_white_space > 0 ? total_white_space - 1 : 1;
-    }
 
     printf("total lignes: %ld\n", total_lignes);
     printf("total bytes: %ld\n", total_bytes);
@@ -80,5 +97,3 @@ int main(int argc, char** argv) {
 //     gb_printf_err("%s%s\n", NORMAL_COLOR, strerror(errno));
 //     gb_exit(1);
 // }
-
-
